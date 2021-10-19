789e24dc0af6ebd3f352170f2d7cd73a87b58312
dondev1992 <donsosa1@gmail.com>
2021-10-19 12:15:04 -0400

    Completed initial version of site

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+  env: {
+    API_KEY: "c986b87d07a24b0a7c5aeef9a00b0545",
+  },
+};
